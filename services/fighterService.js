116edd8f36a27cc782423fd54fa6f74473697920
ysilkov <65254365+ysilkov@users.noreply.github.com>
2022-06-13 12:32:42 +0300

    Update fighterService.js

--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,68 @@
 const { FighterRepository } = require('../repositories/fighterRepository');
 
 class FighterService {
-    // TODO: Implement methods to work with fighters
-}
+        create(fighterData) {
+            const {
+                name,
+                health,
+                power,
+                defense,
+            } = fighterData;
+    
+            // Is fighter exist?
+            const item = this.search({ name });
+    
+            if (item) {
+                return null;
+            }
+    
+            return FighterRepository.create({
+                name,
+                health,
+                power,
+                defense,
+            });
+        }
+    
+        read() {
+            return FighterRepository.getAll();
+        }
+    
+        update(id, fighterData) {
+            const {
+                name,
+                health,
+                power,
+                defense,
+            } = fighterData;
+    
+            // Is user exist?
+            const item = this.search({ id });
+    
+            if (!item) {
+                return null;
+            }
+    
+            return FighterRepository.update(id, {
+                name,
+                health,
+                power,
+                defense,
+            });
+        }
+    
+        delete(id) {
+            return FighterRepository.delete(id);
+        }
+    
+        search(search) {
+            const item = FighterRepository.getOne(search);
+            if(!item) {
+                return null;
+            }
+            return item;
+        }
+    }
+    
 
 module.exports = new FighterService();
