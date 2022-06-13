042a71d52a3d6fde79b20282c4317558ff9b1d71
ysilkov <65254365+ysilkov@users.noreply.github.com>
2022-06-13 12:31:46 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,110 @@ const { createUserValid, updateUserValid } = require('../middlewares/user.valida
 const { responseMiddleware } = require('../middlewares/response.middleware');
 
 const router = Router();
+router.use(createUserValid);
+router.use(updateUserValid);
 
-// TODO: Implement route controllers for user
+router.get('/', (res, req, next)=>{
+    try{
+        const result = UserService.read();
+        if(!result){
+            throw new Error("Users are not define");
+        }
+        res.data = result;
+    } catch(error){
+        error.type =" not found";
+        res.err = error;
+    }finally{
+        next();
+    }
+    res.data = {};
+    next();
+});
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+  
+    try {
+      const result = UserService.search({ id });
+      if (!result) {
+        throw new Error('User not found');
+      }
+      res.data = result;
+    } catch (error) {
+      error.type = 'not_found';
+      res.err = error;
+    } finally {
+      next();
+    }
+    res.data = {};
+    next();
+  });
+  
+  // Create user
+  router.post('/', (req, res, next) => {
+    if (req.err) {
+      return next();
+    }
+  
+    const userData = req.body;
+    
+    try {
+      const result = UserService.create(userData);
+      if (!result) {
+        throw new Error('User already exist');
+      }
+      res.data = {};
+    } catch (error) {
+      error.type = 'create';
+      res.err = error;
+    } finally {
+      next();
+    }
+  });
+  
+  // Update user
+  router.put('/:id', (req, res, next) => {
+    if (req.err) {
+      return next();
+    }
+    
+    const { id } = req.params;
+    const userData = req.body;
+  
+    try {
+      const result = UserService.update(id, userData);
+      if (!result) {
+        throw new Error('User entity to update is not exist');
+      }
+      res.data = {};
+    } catch (error) {
+      error.type = 'not_found';
+      res.err = error;
+    } finally {
+      next();
+    }
+    res.data = {};
+    next();
+  });
+  
+  
+  // Delete user
+  router.delete('/:id', (req, res, next) => {
+    const { id } = req.params;
+    
+    try {
+      const result = UserService.delete(id);
+      if (!result.length) {
+        throw new Error('User entity to delete is not exist');
+      }
+      res.data = {};
+    } catch (error) {
+      error.type = 'delete';
+      res.err = error;
+    } finally {
+      next();
+    }
+  });
+
+  router.use(responseMiddleware);
 
 module.exports = router;
