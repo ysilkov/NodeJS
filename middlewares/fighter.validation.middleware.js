e418c1f019b9b9db2546ef818c25072e44e57f54
ysilkov <65254365+ysilkov@users.noreply.github.com>
2022-06-13 12:34:42 +0300

    Update fighter.validation.middleware.js

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,14 +1,67 @@
 const { fighter } = require('../models/fighter');
+const isValidName = (username) => {
+    const regex = /^[a-zA-Z\-]+$/;
+    return !!username && regex.test(username);
+}
+
+const isValidPower = (power) => {
+    return typeof(power) === 'number' && power < 100;
+}
+const compareLists = (a, b) => a.filter(v => !b.includes(v));
+
+const validate = (fighterData) => {
+    const { name, power } = fighterData;
+
+    const requestFields = Object.keys(fighterData);
+    const modelFields = Object.keys(fighter);
+
+    const fieldDifference = compareLists(requestFields, modelFields);
+    
+    const validations = [
+        !fieldDifference.length,
+        !requestFields.includes('id'),
+        isValidName(name),
+        isValidPower(power),
+    ];
+    
+    return validations.every(item => item === true);
+}
 
 const createFighterValid = (req, res, next) => {
-    // TODO: Implement validatior for fighter entity during creation
+    if (req.method !== 'POST') {
+        return next();
+    }
+
+    const fighterData = req.body;
+
+    const isValid = validate(fighterData);
+    
+    if (!isValid) {
+        const err = new Error('Fighter entity to create isn\'t valid');
+        err.type = 'validation';
+        res.err = err;
+    }
+
     next();
 }
+
 
 const updateFighterValid = (req, res, next) => {
-    // TODO: Implement validatior for fighter entity during update
+    if (req.method !== 'PUT') {
+        return next();
+    }
+
+    const fighterData = req.body;
+
+    const isValid = validate(fighterData);
+    
+    if (!isValid) {
+        const err = new Error('Fighter entity to update isn\'t valid');
+        err.type = 'validation';
+        res.err = err;
+    }
+
     next();
 }
-
 exports.createFighterValid = createFighterValid;
 exports.updateFighterValid = updateFighterValid;
