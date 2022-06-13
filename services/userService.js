f3993ad83f7591a9897c825bc875f4e4c9b230e8
ysilkov <65254365+ysilkov@users.noreply.github.com>
2022-06-13 12:33:42 +0300

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -2,7 +2,64 @@ const { UserRepository } = require('../repositories/userRepository');
 
 class UserService {
 
-    // TODO: Implement methods to work with user
+    create(userData) {
+        const {
+            firstName,
+            lastName,
+            email,
+            phoneNumber,
+            password,
+        } = userData;
+
+        // Is user exist?
+        const item = this.search({ email });
+
+        if (item) {
+            return null;
+        }
+
+        return UserRepository.create({
+            firstName,
+            lastName,
+            email,
+            phoneNumber,
+            password,
+        });
+    }
+
+    read() {
+        return UserRepository.getAll();
+    }
+
+    update(id, userData) {
+        const {
+            firstName,
+            lastName,
+            email,
+            phoneNumber,
+            password,
+        } = userData;
+
+        // Is user exist?
+        const item = this.search({ id });
+
+        if (!item) {
+            return null;
+        }
+
+        return UserRepository.update(id, {
+            firstName,
+            lastName,
+            email,
+            phoneNumber,
+            password,
+        });
+    }
+
+    delete(id) {
+        return UserRepository.delete(id);
+    }
+
 
     search(search) {
         const item = UserRepository.getOne(search);
