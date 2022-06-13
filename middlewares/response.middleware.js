42b60f7951a0a305aba2c2bcb7398702a6660b25
ysilkov <65254365+ysilkov@users.noreply.github.com>
2022-06-13 12:35:12 +0300

    Update response.middleware.js

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,16 @@
 const responseMiddleware = (req, res, next) => {
-   // TODO: Implement middleware that returns result of the query
+   if(res.data){
+       res.status(200).json(res.data);
+       return next();
+   }
+   if(res.err){
+       res.status(code).json({
+        error: true,
+        message: res.err.message
+       });
+       return next();
+   }
+
     next();
 }
 
